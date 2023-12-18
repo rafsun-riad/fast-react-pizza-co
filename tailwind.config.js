02850b655e8e9b3d6053deb2f04eb1a10e8deaf4
rafsun-riad <mruhaquer@gmail.com>
2023-12-18 21:07:45 +0600

    did some more styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
     },
-    plugins: [],
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
+  },
+  plugins: [],
 };
