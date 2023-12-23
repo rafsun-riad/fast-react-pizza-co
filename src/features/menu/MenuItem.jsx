05d16dc472f239192e6a79c5c6edac5a4e7e4396
rafsun-riad <mruhaquer@gmail.com>
2023-12-23 20:58:19 +0600

    worked with cart again

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utilities/helpers';
-import { addItem } from '../cart/cartSlice';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     const newItem = {
@@ -38,6 +41,8 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
+
+          {isInCart && !isInCart && <DeleteItem pizzaId={id} />}
           {!soldOut && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
